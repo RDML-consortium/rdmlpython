14503cfbd071f80e0c1797e64b0ff058bc6404b0
Dwight Kuo <dkuo@alamarbio.comn>
2022-03-02 21:31:30 -0800

    only calculate slope if there were non-nan values

--- rdml.py
+++ rdml.py
@@ -868,9 +868,7 @@ def _lrp_testSlopes(fluor, aRow, stopCyc, startCycFix):
                 sumx2 += i * i
                 sumxy += i * np.log10(fluor[aRow, i - 1])
                 nincl += 1
-        if nincl == 0.0:
-          slope[j] = 0.0
-        else:
+        if nincl != 0.0:
           ssx[j] = sumx2 - sumx * sumx / nincl
           sxy[j] = sumxy - sumx * sumy / nincl
           slope[j] = sxy[j] / ssx[j]
