602a54276a6e1864775fc6f7e55a38b5e7acdccc
Andreas Untergasser <andreas@untergasser.de>
2023-03-10 18:57:02 +0100

    Good stat error message

--- rdml.py
+++ rdml.py
@@ -29,7 +29,7 @@ def get_rdml_lib_version():
         The version string of the RDML library.
     """
 
-    return "1.7.1"
+    return "1.7.2"
 
 
 class NpEncoder(json.JSONEncoder):
@@ -1879,6 +1879,13 @@ def _getXMLPartitionDataType():
 
 def runStatistics(statTarGroup, parametric):
     ret = {}
+    if len(statTarGroup) < 2:
+        ret["test name"] = "no test possible"
+        ret["stat name"] = "only 1 group"
+        ret["stat val"] = -1.0
+        ret["p val"] = -1.0
+        return ret
+
     if _string_to_bool(parametric, triple=False):
         if len(statTarGroup) == 2:
             ret["test name"] = "T-test"
@@ -1887,11 +1894,7 @@ def runStatistics(statTarGroup, parametric):
         else:
             ret["test name"] = "One-way ANOVA"
             ret["stat name"] = "F statistic"
-            if len(statTarGroup) > 2:
-                ret["stat val"], ret["p val"] = scp.f_oneway(*statTarGroup)
-            else:
-                ret["stat val"] = -1.0
-                ret["p val"] = -1.0
+            ret["stat val"], ret["p val"] = scp.f_oneway(*statTarGroup)
     else:
         if len(statTarGroup) == 2:
             ret["test name"] = "Mann-Whitney U rank test"
@@ -1900,11 +1903,7 @@ def runStatistics(statTarGroup, parametric):
         else:
             ret["test name"] = "Kruskal-Wallis H-test"
             ret["stat name"] = "H statistic"
-            if len(statTarGroup) > 2:
-                ret["stat val"], ret["p val"] = scp.kruskal(*statTarGroup)
-            else:
-                ret["stat val"] = -1.0
-                ret["p val"] = -1.0
+            ret["stat val"], ret["p val"] = scp.kruskal(*statTarGroup)
     return ret
 
 def webAppRunStatistics(data, parametric=False, seperator='\t', replaceComma=True):
